d26be1325fa95afccd6ba923d56e6de8d86c4f7c
rheachandok12 <67009074+rheachandok12@users.noreply.github.com>
2020-06-20 03:48:31 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>The covid 19 search engine</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="app.css">
+	</head>
+<body>
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+<div class="container">
+	<div class="row">
+		<div class="col-lg-12">
+		<div id="content">
+
+
+		<h1><strong>CORONAVIRUS:</strong> EVERYTHING THERE IS TO KNOW</h1>
+		<h4>Find answers to all your Covid-19 related queries here</h4>
+		</div>
+	</div>
+</div>
+</div>
+
+
+  
+
+
+
+<div class="searchbar">
+
+				<form action="rhea.php" method="post">
+
+					<input class="box" type="text" name="name" placeholder="Enter your Covid-19 based query">
+					<input class="Button1" type="submit" value="Search">
+
+
+				</form>
+
+
+			</div>
+
+
+
+</body>
+</html>
